7e67c1430298ebfb6d557b28712172a0731b4593
Andi Fahmi <andifahmi266@gmail.com>
2023-05-28 00:04:43 +0800

    Tidy Up

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -9,7 +9,7 @@ const Search = () => {
           Any week
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-          <div className="hidden sm:block">Add Guest</div>
+          <div className="hidden sm:block">AddGuest</div>
           <div className="p-2 bg-rose-500 rounded-full text-white">
             <BiSearch size={18} />
           </div>
