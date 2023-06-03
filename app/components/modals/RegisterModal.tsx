11221aafefd0b6baf0985a0b8aa240e29ce5a170
Andi Fahmi <andifahmi266@gmail.com>
2023-06-04 07:50:37 +0800

    Tidy Up

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -92,7 +92,7 @@ const RegisterModal = () => {
       />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="flex flex-row items-center gap-2 justify-center">
-          <div>Already Have an Account?</div>
+          <div>Already have an account?</div>
           <div
             onClick={registerModal.onClose}
             className="text-neutral-800 cursor-pointer hover:underline"
